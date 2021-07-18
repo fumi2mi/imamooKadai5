3dbd82ffb813d10fc66069e4ad4f809ac72c9ff8
akio0911 <akio0911@gmail.com>
2021-07-18 18:04:10 +0900

    feedback

--- imamooKadai5/CalculatorViewController.swift
+++ imamooKadai5/CalculatorViewController.swift
@@ -13,24 +13,32 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak private var resultLabel: UILabel!
 
     @IBAction private func buttonPressed(_ sender: UIButton) {
-        if dividendTextField.text == "" {
+        guard let dividendNumber = Double(dividendTextField.text ?? "") else {
             displayAlert(message: "割られる数を入力してください")
-        } else if divisorTextField.text == "" {
-            displayAlert(message: "割る数を入力してください")
-        } else if divisorTextField.text == "0" {
-            displayAlert(message: "割る数には0を入力しないでください")
-        } else {
-            let dividendNumber = Double(dividendTextField.text ?? "") ?? 1
-            let divisorNumber = Double(divisorTextField.text ?? "") ?? 1
-            resultLabel.text = String(format: "%.5f", dividendNumber / divisorNumber)
+            return
         }
+
+        guard let divisorNumber = Double(divisorTextField.text ?? "") else {
+            displayAlert(message: "割る数を入力してください")
+            return
+        }
+
+        guard divisorNumber != 0 else {
+            displayAlert(message: "割る数には0を入力しないでください")
+            return
+        }
+
+        resultLabel.text = String(format: "%.5f", dividendNumber / divisorNumber)
     }
 
     private func displayAlert(message: String) {
         let title = "課題5"
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let ok = UIAlertAction(title: "OK", style: .default, handler: nil)
-        alert.addAction(ok)
+
+        // Identifier Name Violation: Variable name should be between 3 and 40 characters long: 'ok' (identifier_name)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+
+        alert.addAction(okAction)
         present(alert, animated: true, completion: nil)
     }
 }
